5805617763ba31a07627c3a14f26aa9520d0e680
自分のアカウント名 <>
2022-09-17 13:45:34 +0900

    enum

---
+++ Enum.py
@@ -0,0 +1,23 @@
+from enum import Enum
+class Shingou(Enum):
+    RED = 1
+    BLUE = 2
+    YELLOW = 3
+
+def act_shingou(color):
+    if color == 1:
+        print("とまれ")
+    elif color == 2:
+        print("すすめ")
+    elif color == 3:
+        print("ちゅうい")
+    else :
+        raise Exception("信号機の色に対応していません")
+
+# shingou = Shingou(1)
+# if shingou is Shingou.RED:
+#     elif shingou is Shingou.BLUE:
+# print(Shingou.name)
+# print(Shingou.value)
+# for data in Shingou:
+#     print(data.value)
